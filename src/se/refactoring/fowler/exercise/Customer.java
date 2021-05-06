80258aefeea8fe7a409a950da715eab0031ecf28
liza-kl <l.kletsko0202@gmail.com>
2021-05-06 13:17:38 +0200

    Used the Replace Temp With Query Refacotring to replace TotalCharge and FrequentRenterPoints

--- src/se/refactoring/fowler/exercise/Customer.java
+++ src/se/refactoring/fowler/exercise/Customer.java
@@ -17,7 +17,25 @@ class Customer {
         return name;
     }
 
+    private double getTotalCharge() {
+        double result = 0;
+        Enumeration enum_rentals = rentals.elements();
+        while (enum_rentals.hasMoreElements()) {
+            Rental each = (Rental) enum_rentals.nextElement();
+            result += each.getCharge();
+        }
+        return result;
+    }
 
+    private int getTotalFrequentRenterPoints() {
+        int result = 0;
+        Enumeration enum_rentals = rentals.elements();
+        while (enum_rentals.hasMoreElements())  {
+            Rental each = (Rental) enum_rentals.nextElement();
+            result += each.getFrequentRenterPoints();
+        }
+        return result;
+    }
 
     public String statement() {
         double totalAmount = 0;
@@ -27,18 +45,20 @@ class Customer {
 
         while (enum_rentals.hasMoreElements()) {
             Rental each = (Rental) enum_rentals.nextElement();
-            // add frequent renter points
             frequentRenterPoints += each.getFrequentRenterPoints();
+
             //show figures for this rental
             result += "\t" + each.getMovie().getTitle()+ "\t" + "\t" + each.getDaysRented() + "\t"
                     + String.valueOf(each.getCharge()) + "\n";
             totalAmount += each.getCharge();
         }
         // add footer lines
-        result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
-        result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
+        result += "Amount owed is " + String.valueOf(getTotalCharge()) + "\n";
+        result += "You earned " + String.valueOf(getTotalFrequentRenterPoints()) + " frequent renter points";
         return result;
     }
+
+
 
 
 
