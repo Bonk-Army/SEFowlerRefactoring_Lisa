0fb9cb271c8dc1cfc8e4241b66a027ae533d5c20
liza-kl <l.kletsko0202@gmail.com>
2021-05-06 13:09:57 +0200

    Extracted frequentRenterPoints Method into Rental Class

--- src/se/refactoring/fowler/exercise/Rental.java
+++ src/se/refactoring/fowler/exercise/Rental.java
@@ -27,6 +27,15 @@ class Rental {
         return result;
     }
 
+    int getFrequentRenterPoints() {
+        if((getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
+        getDaysRented() > 1) {
+            return 2;
+        } else {
+            return 1;
+        }
+    }
+
     public int getDaysRented() {
         return daysRented;
     }
