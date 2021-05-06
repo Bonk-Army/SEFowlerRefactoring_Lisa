08b98d7ba911b92e97c59ac9e950533c4d7e5e94
liza-kl <l.kletsko0202@gmail.com>
2021-05-06 14:11:04 +0200

    Extracted the getCharge Method into abstract Class Price, Replace Conditional with Polymorphism

--- src/se/refactoring/fowler/exercise/Price.java
+++ src/se/refactoring/fowler/exercise/Price.java
@@ -3,23 +3,7 @@ package se.refactoring.fowler.exercise;
 abstract class Price {
     abstract int getPriceCode();
 
-    double getCharge(int daysRented) {
-        double result = 0;
-        switch (getPriceCode()) {
-            case Movie.REGULAR -> {
-                result += 2;
-                if (daysRented > 2)
-                    result += (daysRented - 2) * 1.5;
-            }
-            case Movie.NEW_RELEASE -> result += daysRented * 3;
-            case Movie.CHILDRENS -> {
-                result += 1.5;
-                if (daysRented > 3)
-                    result += (daysRented - 3) * 1.5;
-            }
-        }
-        return result;
-    }
+    abstract double getCharge(int daysRented);
 
 }
 
